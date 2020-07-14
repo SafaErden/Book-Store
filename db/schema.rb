15cfb34c369472997ef1be2ec07566783c5e522b
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-07-14 18:25:46 +0300

    add scaffold author

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_07_14_151916) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
