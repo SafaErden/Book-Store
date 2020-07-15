4fed77c62a74e3d8ba44df6d1cb71a2aafc41849
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-07-15 21:33:23 +0300

    add style for library show page

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -24,10 +24,6 @@ class LibrariesController < ApplicationController
     @library = Library.new
   end
 
-  # GET /libraries/1/edit
-  def edit
-  end
-
   # POST /libraries
   # POST /libraries.json
   def create
@@ -41,30 +37,6 @@ class LibrariesController < ApplicationController
         format.html { render :new }
         format.json { render json: @library.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /libraries/1
-  # PATCH/PUT /libraries/1.json
-  def update
-    respond_to do |format|
-      if @library.update(library_params)
-        format.html { redirect_to @library, notice: 'Library was successfully updated.' }
-        format.json { render :show, status: :ok, location: @library }
-      else
-        format.html { render :edit }
-        format.json { render json: @library.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /libraries/1
-  # DELETE /libraries/1.json
-  def destroy
-    @library.destroy
-    respond_to do |format|
-      format.html { redirect_to libraries_url, notice: 'Library was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
