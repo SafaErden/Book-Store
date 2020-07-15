e4863bf03ab252088ec8cba2b6c354530279bdd9
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-07-15 20:31:08 +0300

    create navbar partial

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,18 +11,10 @@ class BooksController < ApplicationController
     @books = Book.all.select{|book| book.author != current_author}
   end
 
-  # GET /books/1
-  # GET /books/1.json
-  def show
-  end
 
   # GET /books/new
   def new
     @book = Book.new
-  end
-
-  # GET /books/1/edit
-  def edit
   end
 
   # POST /books
@@ -35,34 +27,10 @@ class BooksController < ApplicationController
       if @book.save
         format.html { redirect_to books_path, notice: 'Book was successfully created.' }
         format.json { render :show, status: :created, location: @book }
-      else
+      else 
         format.html { render :new }
         format.json { render json: @book.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /books/1
-  # PATCH/PUT /books/1.json
-  def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
-        format.json { render :show, status: :ok, location: @book }
-      else
-        format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /books/1
-  # DELETE /books/1.json
-  def destroy
-    @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
