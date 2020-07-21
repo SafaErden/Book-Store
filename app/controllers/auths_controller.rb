f991801016e8c5f0683b30890e1cdabc09a62213
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-07-21 12:32:18 +0300

    move sign in logic from auth controller to auth helper

--- app/controllers/auths_controller.rb
+++ app/controllers/auths_controller.rb
@@ -1,20 +1,11 @@
 class AuthsController < ApplicationController
+  include AuthsHelper
   def new
     redirect_to current_author if logged_in?
   end
 
   def create
-    @author = Author.find_by(name: params[:name])
-
-    if @author
-      session[:author_id] = @author.id
-      redirect_to @author
-    else
-      respond_to do |format|
-        alert_text = 'Sorry, we have no author with this input. Please sign up, if you don\'t have an account.'
-        format.html { redirect_to new_auth_path, alert: alert_text }
-      end
-    end
+    sign_in
   end
 
   def destroy
