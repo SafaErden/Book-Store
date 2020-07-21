aeb317eb75da0b3bdbd7922f1561f1046b02e957
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-07-21 13:37:57 +0300

    add new tests for updated validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,16 +3,12 @@ class Book < ApplicationRecord
   has_many :book_libraries
   has_many :libraries, through: :book_libraries
 
-  validate :name_length
+  validates :name, presence: true, length: { minimum: 2, message: 'should have at least 2 characters.' }
   validate :greater_than_zero
 
   scope :desc, -> { order('created_at DESC') }
 
   def greater_than_zero
     errors.add(:amount, 'should be greater than 0') if amount.to_i < 1
-  end
-
-  def name_length
-    errors.add(:name, 'should contain at least two characters') if name.size < 2
   end
 end
